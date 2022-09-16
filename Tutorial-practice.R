3c66df5fce30635b06d355fe3a4a69c456268be4
JasmineAKD <jadhillo@sfu.ca>
2022-09-16 11:54:37 -0700

    Lab 1 September 16 2022

---
+++ Tutorial-practice.R
@@ -0,0 +1,9 @@
+to_print = "hello world" #comments are useful so get used to using them :)
+print(to_print)
+#print(nonexistent_variable)
+  #the line above is code that has been converted to a comment
+    #converting code to a comment is called "commenting out"
+  #what happens to the code if it is uncommented?
+    #there will be an error since "nonexistent_variable" is not defined
+existent_variable <- "this is a test"
+print(existent_variable)
